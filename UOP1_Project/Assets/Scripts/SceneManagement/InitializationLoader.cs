0fd0d76278c5f038197924151c3c3b21d1c21aa7
mfornshell <79386976+mfornshell@users.noreply.github.com>
2024-02-13 23:09:59 -0600

    refactor InitializationLoader

--- UOP1_Project/Assets/Scripts/SceneManagement/InitializationLoader.cs
+++ UOP1_Project/Assets/Scripts/SceneManagement/InitializationLoader.cs
@@ -1,37 +1,26 @@
-﻿using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceProviders;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This class is responsible for starting the game by loading the persistent managers scene 
 /// and raising the event to load the Main Menu
 /// </summary>
-
 public class InitializationLoader : MonoBehaviour
 {
-	[SerializeField] private GameSceneSO _managersScene = default;
-	[SerializeField] private GameSceneSO _menuToLoad = default;
+	[SerializeField] private GameSceneSO _persistentManagersScene;
+	[SerializeField] private GameSceneSO _mainMenuScene;
 
 	[Header("Broadcasting on")]
-	[SerializeField] private AssetReference _menuLoadChannel = default;
+	[SerializeField] LoadEventChannelSO _loadMenu;
 
-	private void Start()
+	IEnumerator Start()
 	{
 		//Load the persistent managers scene
-		_managersScene.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true).Completed += LoadEventChannel;
-	}
-
-	private void LoadEventChannel(AsyncOperationHandle<SceneInstance> obj)
-	{
-		_menuLoadChannel.LoadAssetAsync<LoadEventChannelSO>().Completed += LoadMainMenu;
-	}
-
-	private void LoadMainMenu(AsyncOperationHandle<LoadEventChannelSO> obj)
-	{
-		obj.Result.RaiseEvent(_menuToLoad, true);
-
+		yield return _persistentManagersScene.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
+		LoadMainMenu();
 		SceneManager.UnloadSceneAsync(0); //Initialization is the only scene in BuildSettings, thus it has index 0
 	}
+
+	void LoadMainMenu() => _loadMenu.RaiseEvent(_mainMenuScene, true);
 }
