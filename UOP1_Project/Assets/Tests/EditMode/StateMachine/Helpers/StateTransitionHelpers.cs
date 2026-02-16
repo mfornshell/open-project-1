bb987d7863b6eaf1827537ca485ee68d6f8f5ffe
mfornshell <79386976+mfornshell@users.noreply.github.com>
2026-02-16 12:44:58 -0600

    add StateTests

---
+++ UOP1_Project/Assets/Tests/EditMode/StateMachine/Helpers/StateTransitionHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UOP1.StateMachine;
+
+namespace UOP1.Tests
+{
+	internal static class StateTransitionHelpers
+	{
+		internal static StateTransition Create(bool returnsTrue = true)
+		{
+			var condition = new StateCondition(null, new MockCondition(true), returnsTrue);
+			return new StateTransition(new State(), new StateCondition[] { condition });
+		}
+	}
+}
---
+++ UOP1_Project/Assets/Tests/EditMode/StateMachine/StateTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using UOP1.StateMachine;
+
+namespace UOP1.Tests
+{
+	public class StateTests
+	{
+		[Test]
+		public void TryGetTransition_CanTransition_ReturnsTrueWithValidState()
+		{
+			var transitions = Enumerable.Repeat(StateTransitionHelpers.Create(), 5);
+
+			var state = new State(null, null, transitions.ToArray(), null);
+
+			var actual = state.TryGetTransition(out var s);
+
+			Assert.True(actual);
+			Assert.NotNull(s);
+		}
+
+		[Test]
+		public void TryGetTransition_CanNotTransition_ReturnsFalseWithNullState()
+		{
+			var transitions = Enumerable.Repeat(StateTransitionHelpers.Create(false), 5);
+
+			var state = new State(null, null, transitions.ToArray(), null);
+
+			var actual = state.TryGetTransition(out var s);
+
+			Assert.False(actual);
+			Assert.Null(s);
+		}
+	}
+}
